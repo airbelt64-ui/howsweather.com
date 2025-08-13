872c342915df01e719f2529a1913ce7cacae1fc2
airbelt64-ui <airbelt64@gmail.com>
2025-08-13 17:09:07 +0900

    Update weather.get.ts

--- pages/weather.get.ts
+++ pages/weather.get.ts
@@ -1,1 +1,12 @@
 
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  const city = query.city || 'Seoul'
+  const apiKey = process.env.NUXT_PUBLIC_OPENWEATHER_API_KEY
+
+  const response = await $fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  )
+
+  return response
+})

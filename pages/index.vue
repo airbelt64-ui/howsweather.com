3dc476ff46666ce13fa1afec241c5a94da668ed8
airbelt64-ui <airbelt64@gmail.com>
2025-08-13 11:05:37 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
+  <main style="font-family:system-ui;padding:24px">
     <h1>HowsWeather.com</h1>
-    <p>Nuxt 3 테스트 페이지</p>
-  </div>
+    <p>Nuxt 3 테스트 페이지 – 배포 확인용</p>
+  </main>
 </template>
